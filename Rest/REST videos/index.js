0e3f01bdb96575a50e0c9411eb8c69bc7fed8ef7
coderroot2 <udayraut128@gmail.com>
2024-04-09 23:18:14 +0530

    add posts and new

--- Rest/REST videos/index.js
+++ Rest/REST videos/index.js
@@ -4,6 +4,8 @@ const port = 8080;
 const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -32,6 +34,23 @@ app.get("/", (req, res) => {
 app.get("/posts" , (req,res) =>{
      res.render("index.ejs" ,{posts});
 })
+
+
+app.get("/posts/new",(req,res)=>{
+res.render("new.ejs");
+});
+
+app.post("/posts", (req,res)=>{
+  let { username, content } = req.body;
+ posts.push({username, content});
+ res.redirect("/posts");
+ 
+
+});
+
+
+
+
 
 app.listen(port, () => {
   console.log("listening to port :8080");
