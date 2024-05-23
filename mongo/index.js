4b5c520205c5d4f7fbf4dd0b35e9ff0c0219c2ba
coderroot2 <udayraut128@gmail.com>
2024-05-24 05:12:35 +0530

    connection to db

--- mongo/index.js
+++ mongo/index.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+// mongoose.connect("mongodb://127.0.0.1:27017/test");
+// alternative for upper
+
+main().then((res) =>{
+    console.log("connected to db");
+})
+
+.catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
