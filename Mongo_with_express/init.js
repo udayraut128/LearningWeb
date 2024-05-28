dbe79005104331522d2cffb40564dcb9a392e1b4
coderroot2 <udayraut128@gmail.com>
2024-05-29 00:26:41 +0530

    add init content

--- Mongo_with_express/init.js
+++ Mongo_with_express/init.js
@@ -13,13 +13,46 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
-let chat1 = new Chat({
-  from: "uday",
-  to: "sai",
-  msg: "hello",
-  created_at: new Date(),
-});
 
-chat1.save().then((res) => {
-  console.log(res);
-});
+let allChats = [
+  {
+    from: "uday",
+    to: "rahul",
+    msg: "hello bro how are you",
+    created_at: new Date(),
+  },
+  {
+    from: "satish",
+    to: "uday",
+    msg: "wlecome to my home",
+    created_at: new Date(),
+  },
+  {
+    from: "uday",
+    to: "satish",
+    msg: "thank you ",
+    created_at: new Date(),
+  },
+  {
+    from: "rahul",
+    to: "satish",
+    msg: "hello, satish where are you",
+    created_at: new Date(),
+  },
+  {
+    from: "satish",
+    to: "rahu;",
+    msg: "hello, i am in janakpur",
+    created_at: new Date(),
+  },
+  {
+    from: "rahul",
+    to: "uday",
+    msg: "hello bye",
+    created_at: new Date(),
+  },
+];
+
+Chat.insertMany(allChats);
+
+ 
