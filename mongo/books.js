a7692557822d0e31eb16920d644308a8898be76e
coderroot2 <udayraut128@gmail.com>
2024-05-29 22:11:40 +0530

    Update books.js

--- mongo/books.js
+++ mongo/books.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-// mongoose.connect("mongodb://127.0.0.1:27017/test");
-// alternative for upper
+ 
 
 main().then((res) =>{
     console.log("connected to db");
@@ -9,4 +8,54 @@ main().then((res) =>{
 .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  await mongoose.connect("mongodb://127.0.0.1:27017/amazon");
+}
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength:20,
+  },
+  author: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    min:[1,"Price is too low"],
+  },
+  disscount: {
+    type: Number,
+    default: 0,
+  },
+  category:{
+    type:String,
+    enum:["fiction","non-fiction"],
+  },
+});
+
+const Book = mongoose.model("Book",bookSchema);
+
+// const book1 = new Book({
+//     title:"The Alchemist",
+//     author:"Paulo Coelho",
+//     price: 200,
+//     category:"fiction",
+// });
+
+// book1.save().then((res) =>{
+//     console.log(res);
+
+// }).catch(err=>{
+//     console.log(err)
+
+// });
+
+
+Book.findByIdAndUpdate("6655249210ed3ca77fd29ea2",{price:-100} ,{runValidators:true})
+.then((res) =>{
+    console.log(res);
+})
+.catch((err) => {
+    console.log(err.errors.price.properties.message);
+});
